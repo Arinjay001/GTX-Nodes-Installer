0520fd2a26815ed297185837561a418e090c4ad0
Arinjay001 <arinjayostwal536@gmail.com>
2026-06-24 00:23:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
-export email="ostwalarinjay1@gmail.com"
-export user_email="ostwalarinjay1@gmail.com"
-export user_username="Arinjay01"
-export user_firstname="Arinjay"
-export user_lastname="Ostwal"
-export user_password="Arinjay@001"
+echo "GTX Nodes Full Auto Panel Installer"
 
-export FQDN="panel.gtxnodes.xyz"
-export timezone="Asia/Kolkata"
-export CONFIGURE_LETSENCRYPT="true"
-export CONFIGURE_FIREWALL="true"
+bash <(curl -s https://pterodactyl-installer.se) <<EOF
+0
+ostwalarinjay1@gmail.com
+Arinjay01
+Arinjay
+Ostwal
+Arinjay@001
+Asia/Kolkata
+panel.gtxnodes.xyz
+n
+y
+y
+y
+EOF
 
-curl -sSL https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer/master/install.sh | bash -s -- 0
+echo "Done. Open: https://panel.gtxnodes.xyz"
